5601f3fd2a850778bf4c483cfb085acafecf0a35
haha <haha@haha.com>
2019-06-17 16:48:23 +0800

    clone commit

--- asd/src/main/java/one.java
+++ asd/src/main/java/one.java
@@ -1,6 +1,8 @@
 public class one {
     public static void main(String[] args) {
         System.out.println("first class");
+        System.out.println("first class");
+        System.out.println("first class");
 
     }
 }
